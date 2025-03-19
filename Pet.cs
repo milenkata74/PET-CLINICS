503405162ad13449a99b10c7bb8641937338fa06
milenkata74 <125346200+milenkata74@users.noreply.github.com>
2025-03-19 11:35:29 +0200

    Add files via upload

---
+++ Pet.cs
@@ -0,0 +1,13 @@
+﻿namespace YourProject.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; } // Куче, Котка и т.н.
+        public int Age { get; set; }
+
+        public int OwnerId { get; set; }
+        public User Owner { get; set; }
+    }
+}
