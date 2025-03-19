612e2a2d481df8b5d01ca3671e512c91df30bb30
milenkata74 <125346200+milenkata74@users.noreply.github.com>
2025-03-19 11:35:54 +0200

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,14 @@
+﻿using animal_clinic.Controllers;
+
+namespace YourProject.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        // Един потребител може да има няколко домашни любимеца
+        public List<Pet> Pets { get; set; }
+    }
+}
